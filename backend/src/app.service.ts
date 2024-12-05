b791394d58d93f7f88af6ff7a47226a9176839fe
naveen neelam <neelamaveencs@gmail.com>
2024-12-06 00:52:28 +0530

    file path once

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -5,10 +5,10 @@ import { join } from 'path';
 @Injectable()
 export class AppService {
   private visitCount = 0;
+  private filePath = join(__dirname, '../../frontend/build/index.html');
   serveFrontend(): string {
-    const filePath = join(__dirname, '../../frontend/build/index.html');
     this.visitCount+=1;
     console.log("Visit Count: ",this.visitCount)
-    return readFileSync(filePath, 'utf8');
+    return readFileSync(this.filePath, 'utf8');
   }
 }
