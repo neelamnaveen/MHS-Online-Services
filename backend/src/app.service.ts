7904753cf42ed7541abf549ccaed3fb57fc9575d
naveen neelam <neelamaveencs@gmail.com>
2024-12-06 00:36:08 +0530

    render build ready

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -5,8 +5,10 @@ import { join } from 'path';
 @Injectable()
 export class AppService {
   serveFrontend(): string {
+    let visitCount = 0;
     const filePath = join(__dirname, '../../frontend/build/index.html');
-    console.log(__dirname)
+    visitCount+=1;
+    console.log("Visit Count: ",visitCount)
     return readFileSync(filePath, 'utf8');
   }
 }
