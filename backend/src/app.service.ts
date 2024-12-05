2487acee1b4a73be07aee7bf6fe0e94dc592a813
naveen neelam <neelamaveencs@gmail.com>
2024-12-06 00:51:01 +0530

    visit count

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -4,11 +4,11 @@ import { join } from 'path';
 
 @Injectable()
 export class AppService {
+  private visitCount = 0;
   serveFrontend(): string {
-    let visitCount = 0;
     const filePath = join(__dirname, '../../frontend/build/index.html');
-    visitCount+=1;
-    console.log("Visit Count: ",visitCount)
+    this.visitCount+=1;
+    console.log("Visit Count: ",this.visitCount)
     return readFileSync(filePath, 'utf8');
   }
 }
